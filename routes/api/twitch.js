591c89792e834dd202498f90a4b982ee4383e6bf
Aiden <a1891958@adelaide.edu.au>
2026-05-09 05:22:08 +0000

    owwc

--- routes/api/twitch.js
+++ routes/api/twitch.js
@@ -38,7 +38,6 @@ router.get('/live', async (req, res) => {
     const params = new URLSearchParams();
     params.append('user_login', 'ow_esports');
     params.append('user_login', 'ow_esports_jp');
-    params.append('user_login', 'unter');
 
     const twitchRes = await fetch(`https://api.twitch.tv/helix/streams?${params}`, {
       headers: {

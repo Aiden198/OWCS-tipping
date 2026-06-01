ac3c20c84f29e7cc3e46d345ae8f3a0552d44ba7
Aiden <a1891958@adelaide.edu.au>
2026-06-01 17:13:30 +0000

    new source pages

--- services/liquipedia/sourcePages.js
+++ services/liquipedia/sourcePages.js
@@ -1,11 +1,14 @@
 module.exports = [
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Asia/Stage_1/Korea',
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/EMEA/Stage_1/Relegation',
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/NA/Stage_1/Relegation',
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Champions_Clash',
   'https://liquipedia.net/overwatch/Overwatch_World_Cup/2026',
   'https://liquipedia.net/overwatch/Overwatch_World_Cup/2026/Americas_Online_Qualifier',
   'https://liquipedia.net/overwatch/Overwatch_World_Cup/2026/EMEA_Online_Qualifier',
   'https://liquipedia.net/overwatch/Overwatch_World_Cup/2026/Asia_Online_Qualifier',
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Asia/Stage_1'
+  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Asia/Stage_2/Korea',
+  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/NA/Stage_2',
+  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/EMEA/Stage_2',
+  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/China/Stage_2',
+  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Asia/Stage_2/Japan',
+  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Asia/Stage_2/Pacific',
+  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Midseason_Championship'
+
 ];

1f85f7fb40c7496ec5d074a928a21d2773801487
Aiden <a1891958@adelaide.edu.au>
2026-07-30 11:35:42 +0000

    Source page

--- services/liquipedia/sourcePages.js
+++ services/liquipedia/sourcePages.js
@@ -3,11 +3,6 @@ module.exports = [
   'https://liquipedia.net/overwatch/Overwatch_World_Cup/2026',
 
   // OWCS stage 2
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/NA/Stage_2',
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/EMEA/Stage_2',
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/China/Stage_2',
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Asia/Stage_2/Korea',
-  'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Asia/Stage_2/Pacific',
   'https://liquipedia.net/overwatch/Overwatch_Champions_Series/2026/Midseason_Championship',
 
   // stage 2 relegation
